9939702b0c1203016737b86e055aa32ca8849897
EmLu26 <133606715+EmLu26@users.noreply.github.com>
2023-11-25 20:15:25 +0000

    Updated app.js h1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1>Hello</h1>
+          <h1>Dictionary Cat 🐱 </h1>
         </header>
         <main>
           <Dictionary />
