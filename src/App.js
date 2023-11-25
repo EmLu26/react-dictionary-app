a513c2b739652ce492220743a0515b77d03b65a7
EmLu26 <133606715+EmLu26@users.noreply.github.com>
2023-11-25 14:27:13 +0000

    Checking Bootstrap installed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        <h1>Hello</h1>
+      </header>
+      <button className="btn btn-primary">Test</button>
     </div>
   );
 }
