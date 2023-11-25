bb0552356de706d2f36fdd6000f4d1d80b0d2962
EmLu26 <133606715+EmLu26@users.noreply.github.com>
2023-11-25 20:09:50 +0000

    Added search engine to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
-      <button className="btn btn-primary">Test</button>
+      <div className="container">
+        <header className="App-header">
+          <h1>Hello</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Emma Whitney</small>
+        </footer>
+      </div>
     </div>
   );
 }
