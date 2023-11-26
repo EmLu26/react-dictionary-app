6d2d85f94afa938d186ffff33b4e00617a70332f
EmLu26 <133606715+EmLu26@users.noreply.github.com>
2023-11-26 14:09:49 +0000

    Added axios & dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for "${keyword}" definition`);
+
+    // documentation "https://www.shecodes.io/learn/apis/dictionary"
+    let key = "6798e9t4f3cb9ef4o430784a938ec3f3";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
